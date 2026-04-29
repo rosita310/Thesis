50cbb8b149fd1a38adb569ce3f09d85245fb3718
Morgano Koedam <morganokoedam1996@gmail.com>
2026-04-29 22:01:44 +0200

    Fixed parts that did not work properly. Program.cs will now also generate the necessary input table based on the journal table that is generated by the Python JournalScraper. Currently it runs but does not find articles due to anti-bot measures (Captchas) taken by Elsevier.

--- solution/.net/Elsevier.DownloadArticleMetadata/JournalScraper.cs
+++ solution/.net/Elsevier.DownloadArticleMetadata/JournalScraper.cs
@@ -1,6 +1,6 @@
 ﻿using HtmlAgilityPack;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -44,14 +44,28 @@ namespace Elsevier.DownloadArticleMetadata
             }
         }
 
-        private static string GetArticleInformation(string link)
+        private string GetArticleInformation(string link)
         {
             string url = $"https://sciencedirect.com/{link}";
             Console.WriteLine($"Reading article at: {url}");
-            var web = new HtmlWeb();
-            var doc = web.Load(url);
-            var content = doc.DocumentNode.SelectSingleNode("//script[@type = 'application/json' and @data-iso-key = '_0']").InnerText;
-            return content;
+    
+            // Use the browser we already have open instead of a new web request
+            _driver.Navigate().GoToUrl(url);
+    
+            // Give it a moment to load the JSON script tag
+            Thread.Sleep(3000); 
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(_driver.PageSource);
+    
+            var node = doc.DocumentNode.SelectSingleNode("//script[@type = 'application/json' and @data-iso-key = '_0']");
+    
+            if (node == null)
+            {
+                throw new Exception("Could not find article JSON blob. The site might be blocking us or the layout changed.");
+            }
+
+            return node.InnerText;
         }
 
         private List<string> ProcessJournal(string journalName)
@@ -60,11 +74,13 @@ namespace Elsevier.DownloadArticleMetadata
 
             List<string> articleSubLinks = new List<string>();
             int offset = 0;
-            int show = 100;
+            int show = 25; // Has to be either 25, 50 or 100
 
-            while (true)
+            Boolean go = true;
+
+            while (go)
             {
-                var url = $"https://www.sciencedirect.com/search?pub={journalName.Replace("-", "%20")}&show={show}&sortBy=date&offset={offset}&articleTypes=FLA";
+                var url = $"https://www.sciencedirect.com/search?pub={journalName.Replace(" ", "%20")}&show={show}&sortBy=date&offset={offset}&articleTypes=FLA";
                 _driver.Navigate().GoToUrl(url);
                 //_driver.Manage().Timeouts().PageLoad = new TimeSpan(0, 0, 10);
                 Thread.Sleep(5000);
@@ -75,6 +91,9 @@ namespace Elsevier.DownloadArticleMetadata
                 if (articles is null || articles.Count == 0)
                 {
                     Console.WriteLine("Got no results, so I assume I got everything.");
+                    // Temporary debug: Save the page to see if it's a CAPTCHA
+                    File.WriteAllText("debug_page.html", _driver.PageSource);
+                    Console.WriteLine("Check debug_page.html to see if we were blocked.");
                     break;
                 }
                 Console.WriteLine($"Found {articles.Count} article(s)");
@@ -84,6 +103,7 @@ namespace Elsevier.DownloadArticleMetadata
                     articleSubLinks.Add(link);
                 }
                 offset = offset + show;
+                go = false;
             }
             return articleSubLinks;
         }
@@ -97,13 +117,24 @@ namespace Elsevier.DownloadArticleMetadata
 
         private IWebDriver CreateNewDriver()
         {
-            Console.WriteLine("Create new webdriver");
-            IWebDriver driver;
+            Console.WriteLine("Create new webdriver using Selenium Manager");
+    
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--headless=new"); 
+            // Look more human.
+            options.AddArgument("--disable-blink-features=AutomationControlled");
+            options.AddExcludedArgument("enable-automation");
+            options.AddAdditionalOption("useAutomationExtension", false);
+            options.AddArgument("--window-size=1920,1080");
+            options.AddArgument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
 
-            FirefoxOptions options = new FirefoxOptions();
-            driver = new FirefoxDriver(options);
-            driver.Manage().Timeouts().ImplicitWait = new TimeSpan(0, 0, 30);
-            driver.Manage().Timeouts().PageLoad = new TimeSpan(0, 0, 30);
+            IWebDriver driver = new ChromeDriver(options);
+
+            // Use JavaScript to remove the 'webdriver' flag entirely after launch
+            ((IJavaScriptExecutor)driver).ExecuteScript("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})");
+    
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
 
             return driver;
         }
